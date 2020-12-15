869f9258d72877d5bd6f7000292d3485c258c1e9
fosterv2 <fosterv2@outlook.com>
2020-12-15 15:26:46 -0800

    Change style for ReviewCard component

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="review-card">
-    <p class="rating">Rating: {{ review.rating }}</p>
+    <p class="rating">
+      <span><img :src='fullStar'><img :src='emptyStar'></span>
+      <span>{{ review.user.username }}</span>
+    </p>
+    <p>{{ review.title }}</p>
     <p>{{ review.content }}</p>
   </div>
 </template>
@@ -10,6 +14,12 @@ export default {
   name: 'ReviewCard',
   props: {
     review: Object
+  },
+  data () {
+    return {
+      fullStar: "https://upload.wikimedia.org/wikipedia/commons/e/e5/Full_Star_Yellow.svg",
+      emptyStar: "https://png.pngtree.com/png-clipart/20190617/original/pngtree-vector-star-icon-png-image_3876637.jpg"
+    }
   }
 }
 </script>
@@ -18,7 +28,7 @@ export default {
 <style scoped>
 div {
   width: 50%;
-  margin-left: 24%;
+  margin-left: 24.5%;
   margin-bottom: 10px;
   border: 3px solid #2c3e50;
   border-radius: 10px;
@@ -26,5 +36,12 @@ div {
 
 .rating {
   border-bottom: 2px dotted #2c3e50;
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 3% 5px 3%;
+}
+
+img {
+  height: 20px;
 }
 </style>
