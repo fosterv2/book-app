59e4c687fe4f1b5379b3f9702090e90a7e20e7a8
fosterv2 <fosterv2@outlook.com>
2020-12-18 16:13:18 -0800

    Create component for a review form

---
+++ src/components/ReviewForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="review-form">
+    <h3>My Review for {{ bookName }}</h3>
+    <form>
+      <label>Review Title</label><br/>
+      <input v-model="title" placeholder="Enter review title">
+      <label>Review</label><br/>
+      <input v-model="content" placeholder="Enter review content">
+      <label>Rating</label><br/>
+      <input v-model="rating" type="number" placeholder="Enter review rating">
+      <input type="submit" value="Post Review">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReviewForm',
+  props: {
+    bookName: String
+  },
+  data () {
+    return {
+      title: "",
+      content: "",
+      rating: ""
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
