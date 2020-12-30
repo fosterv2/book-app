0a8f7935c9c43d578b4d274b2d1d17e8198c25f9
fosterv2 <fosterv2@outlook.com>
2020-12-30 13:10:13 -0800

    Add info to the about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,10 @@
   <div class="about">
     <h1>About Page</h1>
     <img src="https://i.imgur.com/YtTSpfV.png">
-    <p>Some info on the app</p>
+    <p>An application for book lovers to share their oppinions about the books they've read.</p>
+    <p>To use the app, a user can look through all the books and reviews with or without signing in.</p>
+    <p>If they want to add a review, they have to sign in or sign up for an account.</p>
+    <p>This app was built using a Ruby on Rails API backend, and a Vue.js frontend.</p>
   </div>
 </template>
 
